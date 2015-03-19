e06802f44350db709f9c5a6d6d0c09f3ed7584b7
Jordeen Chang <jordeenchang@gmail.com>
2015-03-18 21:35:25 -0700

    Initial Commit

---
+++ DrOttr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrOttr
+//
+//  Created by Jordeen Chang on 3/18/15.
+//  Copyright (c) 2015 Jordeen Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrOttr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrOttr
+//
+//  Created by Jordeen Chang on 3/18/15.
+//  Copyright (c) 2015 Jordeen Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

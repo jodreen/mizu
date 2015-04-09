0f442eaba71fb231c438d77e9aa990ee3a1800d7
Jordeen Chang <jordeenchang@gmail.com>
2015-04-08 23:07:07 -0700

    home view

--- DrOttr/DOHomeView.m
+++ DrOttr/DOHomeView.m
@@ -5,17 +5,51 @@
 //  Created by Jordeen Chang on 4/1/15.
 //  Copyright (c) 2015 Jordeen Chang. All rights reserved.
 //
-
 #import "DOHomeView.h"
+#import "UIExtensions.h"
+
+@interface DOHomeView ()
+
+@property (nonatomic) UILabel *titleLabel;
+
+@end
 
 @implementation DOHomeView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        [self createSubviews];
+        [self setNeedsUpdateConstraints];
+        self.backgroundColor = [UIColor blueColor];
+    }
+    return self;
 }
-*/
+
+- (void)createSubviews {
+    
+    _titleLabel = [({
+        UILabel *label = [[UILabel alloc] init];
+        label.text = @"You drank something water today";
+        label.font = [UIFont boldSystemFontOfSize:24.0];
+        label.textAlignment = NSTextAlignmentCenter;
+        label.textColor = [UIColor whiteColor];
+        label;
+    }) do_addToSuperview:self];
+}
+
+- (void)updateConstraints {
+    
+    [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(@(standardMargin));
+        make.leading.equalTo(@(standardMargin));
+        make.trailing.equalTo(@(-standardMargin));
+        make.centerX.equalTo(self.mas_centerX);
+    }];
+    
+    
+    [super updateConstraints];
+}
 
 @end
